1a2f97b6ba05680cdf8077d7cbe6255d41795257
dabeng <dabeng413@gmail.com>
2016-05-24 15:16:22 +0800

    correct the webpack config file

--- src/NewPane.jsx
+++ src/NewPane.jsx
@@ -10,7 +10,7 @@ module.exports = React.createClass({
     return {
       'showMessage': false,
       'message': '',
-      'messageType': '',
+      'messageType': 'info',
       'cmp': '',
       'release': '',
       'contact': '',
